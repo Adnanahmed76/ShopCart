a2843e9bbc3020d38ceb85b423fe89089476af61
adnanahmed504423@gmail.comgit config --global user.email adnanahmed504423@gmail.com <adnanahmed504423@gmail.com>
2025-02-24 23:59:36 +0530

    commit

--- snapcart/lib/utilis/theme/customtheme/checkbox_theme.dart
+++ snapcart/lib/utilis/theme/customtheme/checkbox_theme.dart
@@ -4,7 +4,20 @@ class TCheckBoxTheme{
 
   static CheckboxThemeData lightCheckBoxTheme=CheckboxThemeData(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-    checkColor: MaterialstateProperty.res
+    checkColor: MaterialStateProperty.resolveWith((states){
+      if(states.contains(MaterialState.selected)){
+        return Colors.white;
+      }else{
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateProperty.resolveWith((states){
+      if(states.contains(MaterialState.selected)){
+        return Colors.blue;
+      }else{
+        return Colors.transparent;
+      }
+    })
   )
 }
 
