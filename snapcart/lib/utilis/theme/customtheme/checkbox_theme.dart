363fd8b90a7c8a60045986ee026eefe1b0adcd05
adnanahmed504423@gmail.comgit config --global user.email adnanahmed504423@gmail.com <adnanahmed504423@gmail.com>
2025-02-25 23:22:44 +0530

    commit

--- snapcart/lib/utilis/theme/customtheme/checkbox_theme.dart
+++ snapcart/lib/utilis/theme/customtheme/checkbox_theme.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
-class TCheckBoxTheme{
-
-  static CheckboxThemeData lightCheckBoxTheme=CheckboxThemeData(
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-    checkColor: MaterialStateProperty.resolveWith((states){
-      if(states.contains(MaterialState.selected)){
-        return Colors.white;
-      }else{
-        return Colors.black;
-      }
-    }),
-    fillColor: MaterialStateProperty.resolveWith((states){
-      if(states.contains(MaterialState.selected)){
-        return Colors.blue;
-      }else{
-        return Colors.transparent;
-      }
-    })
-  )
+class TCheckBoxTheme {
+  static CheckboxThemeData lightCheckBoxTheme = CheckboxThemeData(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      checkColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.white;
+        } else {
+          return Colors.black;
+        }
+      }),
+      fillColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.blue;
+        } else {
+          return Colors.transparent;
+        }
+      }));
+  //Customize Dark Text Theme
+  static CheckboxThemeData darkCheckBoxTheme = CheckboxThemeData(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      checkColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.white;
+        } else {
+          return Colors.black;
+        }
+      }),
+      fillColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.blue;
+        } else {
+          return Colors.transparent;
+        }
+      }));
 }
-
