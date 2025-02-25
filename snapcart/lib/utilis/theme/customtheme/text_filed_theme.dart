f550a72d6d5c9d18435d97367fdb257e2a50e175
adnanahmed504423@gmail.comgit config --global user.email adnanahmed504423@gmail.com <adnanahmed504423@gmail.com>
2025-02-25 23:57:20 +0530

    commit

--- snapcart/lib/utilis/theme/customtheme/text_filed_theme.dart
+++ snapcart/lib/utilis/theme/customtheme/text_filed_theme.dart
@@ -1,1 +1,63 @@
+import 'package:flutter/material.dart';
 
+class TTextFormFieldTheme {
+  TTextFormFieldTheme._();
+
+  static InputDecorationTheme lightInputDecorationTheme = InputDecorationTheme(
+      errorMaxLines: 3,
+      prefixIconColor: Colors.grey,
+      suffixIconColor: Colors.grey,
+      labelStyle: TextStyle().copyWith(fontSize: 14),
+      hintStyle: TextStyle().copyWith(fontSize: 14, color: Colors.black),
+      errorStyle: TextStyle().copyWith(
+        fontSize: 12,
+      ),
+      floatingLabelStyle:
+          TextStyle().copyWith(color: Colors.black.withOpacity(0.8)),
+      border: OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: BorderSide(width: 1, color: Colors.grey),
+      ),
+      enabledBorder: OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: BorderSide(width: 1, color: Colors.grey),
+      ),
+      focusedBorder: OutlineInputBorder().copyWith(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(width: 1, color: Colors.black12)),
+      errorBorder: OutlineInputBorder().copyWith(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(width: 1, color: Colors.red)),
+      focusedErrorBorder: OutlineInputBorder().copyWith(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(width: 1, color: Colors.orange)));
+
+  static InputDecorationTheme darkInputDecorationTheme = InputDecorationTheme(
+      errorMaxLines: 3,
+      prefixIconColor: Colors.grey,
+      suffixIconColor: Colors.grey,
+      labelStyle: TextStyle().copyWith(fontSize: 14),
+      hintStyle: TextStyle().copyWith(fontSize: 14, color: Colors.white),
+      errorStyle: TextStyle().copyWith(
+        fontSize: 12,
+      ),
+      floatingLabelStyle:
+          TextStyle().copyWith(color: Colors.white.withOpacity(0.8)),
+      border: OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: BorderSide(width: 1, color: Colors.grey),
+      ),
+      enabledBorder: OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: BorderSide(width: 1, color: Colors.grey),
+      ),
+      focusedBorder: OutlineInputBorder().copyWith(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(width: 1, color: Colors.black12)),
+      errorBorder: OutlineInputBorder().copyWith(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(width: 1, color: Colors.red)),
+      focusedErrorBorder: OutlineInputBorder().copyWith(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(width: 1, color: Colors.orange)));
+}
