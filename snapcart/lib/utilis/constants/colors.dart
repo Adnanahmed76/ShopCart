02209f242f7b6ae7e1afeeb58aff49d11535fca2
adnanahmed504423@gmail.comgit config --global user.email adnanahmed504423@gmail.com <adnanahmed504423@gmail.com>
2025-03-02 23:44:31 +0530

    text Colors

--- snapcart/lib/utilis/constants/colors.dart
+++ snapcart/lib/utilis/constants/colors.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
-class TColors{
+
+class TColors {
   TColors._();
-  static Color primaryColor=Color()
+  //app basic Colors
+  static Color primaryColor = Color(0xFF4b68ff);
+  static Color secondary = Color(0xFFFFE248);
+  static Color accent = Color(0xFFb0c7ff);
+
+  //textColor
+  static Color textPrimary = Color(0xFF333333);
+  static Color textSecondry = Color(0xFF6c7570);
+  static Color textWhite = Colors.white;
+
+  //
 }
