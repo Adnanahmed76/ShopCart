3388fecec44e537d5a9d1e50896d395a35c3b962
adnanahmed504423@gmail.comgit config --global user.email adnanahmed504423@gmail.com <adnanahmed504423@gmail.com>
2025-03-13 22:38:50 +0530

    adding sizes

--- snapcart/lib/utilis/constants/size.dart
+++ snapcart/lib/utilis/constants/size.dart
@@ -8,4 +8,48 @@ class TSizes {
 //icon sizes
   static const double iconXs = 12.0;
   static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+//font sizes
+  static const double fontSizeSm = 14.0;
+  static const double fonstSizeMd = 16.0;
+  static const double fontsizeLg = 18.0;
+
+  //button sizes
+  static const double buttonHeight = 18.0;
+  static const double buttonRadious = 12.0;
+  static const double buttonWidth = 120.0;
+  //appbar height
+  static const double appBarHeight = 56.0;
+
+  //image sizes
+  static const double imageThumbsize = 80.0;
+
+  //default spacing between sections
+  static const double defaultSpace = 24.0;
+  static const double spaceItems = 16.0;
+  static const double spaceBtwSetions = 32.0;
+
+  //border radious
+  static const double borderRadiusSm = 4.0;
+  static const double borderRadiusMd = 8.0;
+  static const double borderRadiusLr = 12.0;
+
+  //divider height
+  static const double dividerHeight = 1.0;
+
+  //product item dimensions
+  static const double productImageSize = 120.0;
+  static const double productImageRadious = 16.0;
+  static const double productItemHeight = 160.0;
+  //input field
+  static const double inputFieldRadious = 12.0;
+  static const double spaceBtwInputFields = 16.0;
+
+// card sizes
+  static const double cardRadiousLg = 16.0;
+  static const double cardRadiousMd = 12.0;
+  static const double cardRadiousSm = 10.0;
+  static const double cardRadiousXs = 6.0;
+  static const double cardElevation = 2.0;
 }
